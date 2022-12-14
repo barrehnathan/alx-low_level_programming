163fdb5d32b7c533f78e6bedc445ef5e2f5794f5
barrehnathan <nathan.nyamai12@gmail.com>
2022-12-14 08:03:35 -0800

    a function that prints the sign of a number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#inlude "main.h"
+
+/**
+ * print_sign -> prints sign based on condition
+ * @n: argument passed
+ * Return: 1, 0 -1
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putcher('0');
+		return (0);
+	}
+	else
+	{

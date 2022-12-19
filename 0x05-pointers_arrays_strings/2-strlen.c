ca13122a6d679c48d38aa115420204283c0fea7d
barrehnathan <nathan.nyamai12@gmail.com>
2022-12-19 04:01:48 -0800

    Task 2 a function that returns the length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - the length of a string
+ * @s: the string
+ * Return: length
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+
+	return (len);
+}

4234f2927bab4cedca2a6cc3ec6fab47b23349ab
barrehnathan <nathan.nyamai12@gmail.com>
2023-01-03 09:24:09 -0800

    Task 1 memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies memory area
+ * @dest: destination pointer
+ * @src: source pointer
+ * @n: number of bytes
+ * Return: destination
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	return (dest);
+}

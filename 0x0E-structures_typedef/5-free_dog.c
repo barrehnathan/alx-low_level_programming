c6a407615541f52605907fe239eca26b57c33216
barrehnathan <nathan.nyamai12@gmail.com>
2023-01-18 13:19:08 -0800

    Task 5

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -13,19 +13,4 @@ void free_dog(dog_t *d)
 	free(d->name);
 	free(d->owner);
 	free(d);
-}#include "dog.h"
-#include <stdlib.h>
-
-/**
- * free_dog - frees dog
- * @d: the dog
- */
-
-void free_dog(dog_t *d)
-{
-	if (d == NULL)
-		return;
-	free(d->name);
-	free(d->owner);
-	free(d);
 }
